703911c048059cbe50d85537604191e51b030ce2
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-27 05:57:31 +0600

    Refactor index.php to improve HTML structure and remove redundant PHP code

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Hello, World! -->
+    <?php
+    echo "Hello, PHP! <br>";
+    echo 'Hello, PHP! <br>';
+    echo "Hello," . ' PHP! ' . "!<br>";
+
+    ?>
+</body>
+
+</html>
 <?php
-echo "I love pizza <br>";
-echo "Its really good ";
-//This is a comment
+echo "Hello, PHP! <br>";
+echo 'Hello, PHP! <br>';
+echo "Hello," . ' PHP! ' . "!<br>";
+
 ?>
