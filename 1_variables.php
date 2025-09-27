23ee52544467dda05a175af0e45f8c74c3343976
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-27 08:14:10 +0600

    Add initial PHP script to display user information

---
+++ 1_variables.php
@@ -0,0 +1,13 @@
+<?php
+$name = "sujon";
+$age = 42;
+$isBrother = true;
+$money = 800;
+
+echo $name . "<br>";
+echo "The number is: " . $age . "<br>";
+echo "Boolean value: " . ($isBrother ? "true" : "false") . "<br>";
+echo "Money: " . $money . "<br>";
+// Don't remove this closing tag
+?>
+<!-- This HTML comment helps keep the PHP closing tag -->
