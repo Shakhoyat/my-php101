fb27aace3a9d5ade6d43bf94e5650c6cce356840
Shakhoyat <shakoyatsujon@gmail.com>
2025-10-02 15:53:05 +0600

    Replace variable examples with string manipulation functions for demonstration

--- 1_variables.php
+++ 1_variables.php
@@ -1,13 +1,11 @@
 <?php
-$name = "sujon";
-$age = 42;
-$isBrother = true;
-$money = 800;
 
-echo $name . "<br>";
-echo "The number is: " . $age . "<br>";
-echo "Boolean value: " . ($isBrother ? "true" : "false") . "<br>";
-echo "Money: " . $money . "<br>";
+$str = "Hello Friend!";
+echo $str . '<br>';
+echo strtolower($str) . '<br>';
+echo strtoupper($str) . '<br>';
+echo str_word_count($str) . '<br>';
+echo strrev($str) . '<br>';
 // Don't remove this closing tag
 ?>
 <!-- This HTML comment helps keep the PHP closing tag -->
